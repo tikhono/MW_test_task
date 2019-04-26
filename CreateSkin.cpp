c53e120a37aaa1489913d387a66ffbb22ef5df06
Tadir <tihonovandrey11@gmail.com>
2019-04-26 13:18:08 +0300

    revert algorithm. start iterating thru spheres and check only points near/inside spheres and cylinders

--- CreateSkin.cpp
+++ CreateSkin.cpp
@@ -1,5 +1,8 @@
 #include "CreateSkin.hpp"
 
+template <typename T>
+T	clamp(T val, T min, T max) {return (std::max(min, std::min(val, max)));}
+
 void 	CreateSkin( const cadcam::mwTPoint3d<double> refPoint,
 					const unsigned long nx, const unsigned long ny,
 					const unsigned long nz, const double sphereRad,
@@ -10,60 +13,80 @@ void 	CreateSkin( const cadcam::mwTPoint3d<double> refPoint,
 	double	start = func.GetBeginParameter();
 	double	end = func.GetEndParameter();
 	auto	pos_count = static_cast<unsigned long>((end - start) / deltaT);
+
 	cadcam::mwTVector3d<double> points[pos_count];
 	double	cur = start;
-	for (int i = 0; i < pos_count; i++)
+	for (unsigned long i = 0; i < pos_count; i++)
 	{
-		points[i] = func.Evaluate(cur);
+		points[i] = func.Evaluate(cur) - refPoint;
 		cur += deltaT;
+	}
+
+	//!Creating two vectors of spheres boundaries mapped in points.
+	cadcam::mwTVector3d<unsigned long> point_min[pos_count];
+	#pragma parallel for
+	for (unsigned long i = 0; i < pos_count; i++)
+	{
+		point_min[i].x(static_cast<unsigned long>(clamp(ceil(points[i].x() - sphereRad), 0.0, static_cast<double>(nx))));
+		point_min[i].y(static_cast<unsigned long>(clamp(ceil(points[i].y() - sphereRad), 0.0, static_cast<double>(ny))));
+		point_min[i].z(static_cast<unsigned long>(clamp(ceil(points[i].z() - sphereRad), 0.0, static_cast<double>(nz))));
+	}
+
+	cadcam::mwTVector3d<unsigned long> point_max[pos_count];
+	#pragma parallel for
+	for (unsigned long i = 0; i < pos_count; i++)
+	{
+		point_max[i].x(static_cast<unsigned long>(clamp(ceil(points[i].x() + sphereRad), 0.0, static_cast<double>(nx))));
+		point_max[i].y(static_cast<unsigned long>(clamp(ceil(points[i].y() + sphereRad), 0.0, static_cast<double>(ny))));
+		point_max[i].z(static_cast<unsigned long>(clamp(ceil(points[i].z() + sphereRad), 0.0, static_cast<double>(nz))));
 	}
 
 	//!Creating squared sphere radius for further calculation to avoid repeating recalculations.
 	const double	sphereSqRad = sphereRad * sphereRad;
 
-	//!creating vector to store skin
-	std::vector < std::vector <unsigned long> > skin(nx, std::vector<unsigned long>(nx, ny));
+	//!Creating vector nx to ny with initial value as top point to store skin
+	std::vector < std::vector <unsigned long> > skin(nx, std::vector<unsigned long>(ny, nz));
 
-	//!NOTE: if sphere moves thru bottom layer, this layer stays on place for better visualisation
-	//!Each Z column starts at first point above bottom one to save bottom layer.
-
-	//!Using OpenMP to parallelize calculation on maximum available CPU cores.
-	#pragma omp parallel for
-	for (unsigned long i = 0; i < nx; i++)
+	//!Deleting points that intersect with spheres
+	//!Iterating thru cube that contain sphere inside.
+	for (unsigned long n = 0; n < pos_count; n++)
 	{
-		for (unsigned long j = 0; j < ny; j++)
+		#pragma parallel for
+		for (auto i = point_min[n].x(); i < point_max[n].x(); i++)
 		{
-			for (unsigned long k = 1; k < nz; k++)
+			for (auto j = point_min[n].y(); j < point_max[n].y(); j++)
 			{
-				//!For reducing memory reads after finding skim point in Z column just start iterating next column.
-				//!Due to restrictions of OpenMP every thread needs own testpoint.
-				//!So, there small increasing of memory usage with testpoint copies.
-				cadcam::mwTVector3d<double> testpoint(i * delta, j * delta, k * delta);
-				bool isDeleted = false;
-				for (unsigned long n = 0; n < pos_count; n++)
+				for (auto k = point_min[n].z(); k < point_max[n].z(); k++)
 				{
-					//!Every testpoint testing on intersection with cylinder and 2 spheres on cylinders caps.
-					//!Second cap always first in next position, so it`s enough to check only first cap and cylinder.
-					//!Ternary expression resolving problem with last position, where need to check only one cap.
-					if ((n == pos_count - 1 ? false :
-						Intersection::Cylinder(refPoint + testpoint, points[n], points[n + 1], sphereSqRad))
-					||	Intersection::Sphere(refPoint + testpoint, points[n], sphereSqRad))
-					{
+					if (skin[i][j] < k)
+						break;
+					cadcam::mwTVector3d<double> testpoint(i * delta, j * delta, k * delta);
+					if (Intersection::Sphere(testpoint, points[n], sphereSqRad))
 						skin[i][j] = k - 1;
-						isDeleted = true;
-						break ;
-					}
-					else if (k == nz - 1)
-					{
-						skin[i][j] = k;
-						break ;
-					}
 				}
-				if (isDeleted)
-					break;
 			}
 		}
 	}
-	//!After finding skim - just write it to the file.
+
+	//!Deleting points that intersect with cylinders
+	//!Iterating thru prism that contain cylinder inside on one of diagonals.
+	for (unsigned long n = 0; n < pos_count - 1; n++)
+	{
+		#pragma parallel for
+		for (auto i = std::min(point_min[n].x(), point_min[n + 1].x()); i < std::max(point_max[n].x(), point_max[n + 1].x()); i++)
+		{
+			for (auto j = std::min(point_min[n].y(), point_min[n + 1].y()); j < std::max(point_max[n].y(), point_max[n + 1].y()); j++)
+			{
+				for (auto k = std::min(point_min[n].z(), point_min[n + 1].z()); k < std::max(point_max[n].z(), point_max[n + 1].z()); k++)
+				{
+					if (skin[i][j] < k)
+						break;
+					cadcam::mwTVector3d<double> testpoint(i * delta, j * delta, k * delta);
+					if (Intersection::Cylinder(testpoint, points[n], points[n + 1], sphereSqRad))
+						skin[i][j] = k - 1;
+				}
+			}
+		}
+	}
 	WriteFile::to(skinFileName, nx, ny, nz, delta, skin);
 }
